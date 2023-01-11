254ea12db8276628c164a42a0972151e7d369558
Garin Mccullick <glmccullick@gmail.com>
2023-01-11 17:09:03 -0600

    added servicer view and changed doc name on all views

--- Lawns_App/resources/views/find_a_yard.blade.php
+++ Lawns_App/resources/views/find_a_yard.blade.php
@@ -5,17 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles.css">
+    <link href="/styles.css" rel="stylesheet">
     <title>Lawns.io</title>
 </head>
 
 <body>
-    <section class="cust-landing-section">
-        <div class="refferal-container">
-            <h3 class="profile-title">Reffer a Friend!</h3>
-            refferal link code
-        </div>
-    </section>
+
 </body>
 
 </html>
